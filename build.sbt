95e1c369fea2472d3db544f74859107949099c0b
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2020-06-30 15:47:15 +0200

    Add junit report generation to sbt

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ scalaVersion := "2.13.3"
 
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.0" % "test"
 libraryDependencies += "org.json4s" %% "json4s-native" % "3.6.9"
+
+testOptions in Test += Tests.Argument(TestFrameworks.ScalaTest, "-u", "junit")
--- src/test/scala/generator/TestStringCombination.scala
+++ src/test/scala/generator/TestStringCombination.scala
@@ -2,9 +2,10 @@ package generator
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import org.scalatest.tagobjects.Slow
 
 class TestStringCombination extends AnyFlatSpec with should.Matchers {
-  "String combination" should "return all possible combination possible" in {
+  "String combination" should "return all possible combination possible" taggedAs Slow in {
     val sc = new stringCombination
     val results = sc.generate(1 to 2)
     results should have size 20
