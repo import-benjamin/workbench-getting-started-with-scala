9510d7e26ef67c5de457db9b4f5ba20e9db7cccf
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2020-09-23 20:46:00 +0200

    Add json JArray manipulation

--- src/test/scala/json/TestJson4s.scala
+++ src/test/scala/json/TestJson4s.scala
@@ -1,8 +1,46 @@
 package json
 
+
+import org.json4s
+import org.json4s.JsonAST.JValue
+import org.json4s.{DefaultFormats, JArray, JObject, JSet, JString, JsonAST}
+import org.json4s.native.JsonMethods
+import org.json4s.native.JsonMethods.{pretty, render}
 import org.scalatest.matchers.should
 import org.scalatest.wordspec.AnyWordSpec
 
-class TestJson4s extends AnyWordSpec with should.Matchers {
 
+class TestJson4s extends AnyWordSpec with should.Matchers {
+  implicit val formats: DefaultFormats.type = DefaultFormats
+
+  "A JArray" when {
+    "inside a JObject" should {
+      val document = JsonMethods.parse(
+        """
+          |{
+          | "document": {
+          |   "content": [
+          |     {
+          |       "id": "hello",
+          |       "content": "some_content"
+          |      },
+          |      {
+          |       "id": "here",
+          |       "content": "some_data"
+          |      },
+          |   ]
+          | }
+          |}
+          |""".stripMargin)
+      "Be convertible into multiples JObject" in {
+        val res = document transformField {
+          case ("content", JArray(s)) => {
+            val list_it: List[(String, JsonAST.JObject)] = (for (JObject(i) <- s) yield ((JObject(i) \ "id").extract[String], JObject(i)))
+            ("content", JObject(list_it))
+          }
+        }
+        println(pretty(render(res)))
+      }
+    }
+  }
 }
