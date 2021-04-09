b0468f5827b6d13ea6fc72bad6f9ff7806b5aa11
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2021-04-09 16:56:38 +0200

    Add manual router

---
+++ src/main/scala/akkaRouter/manualRouter.scala
@@ -0,0 +1,30 @@
+package akkaRouter
+
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
+
+object manualRouter extends App {
+
+  class Master extends Actor {
+
+    private val slaves: IndexedSeq[ActorRefRoutee] = for (index <- 1 to 5) yield {
+      val slave = context.actorOf(Props[Slave], s"slave$index")
+      context.watch(slave)
+      ActorRefRoutee(slave)
+    }
+
+    private val router = Router(RoundRobinRoutingLogic(), slaves)
+
+    override def receive: Receive = {
+      case message => router.route(message, sender())
+    }
+  }
+
+  class Slave extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(s"Receive $message")
+    }
+  }
+
+
+}
