4a6e6a0fecdc37dab550b0bd1579b78665f2908d
Benjamin Boboul <benjamin.boboul@imt-lille-douai.org>
2021-04-13 21:55:14 +0200

    Add actor with custom MailBoxe example

---
+++ src/main/scala/akkaMailBoxes/MailBoxes.scala
@@ -0,0 +1,32 @@
+package akkaMailBoxes
+
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import akka.dispatch.{PriorityGenerator, UnboundedPriorityMailbox}
+import com.typesafe.config.Config
+
+object MailBoxes extends App {
+
+  val system = ActorSystem("MailBoxExample")
+
+  class SimpleActor extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case message => log.info(message.toString)
+    }
+  }
+
+
+  class CustomMailBox(settings: ActorSystem.Settings, config: Config) extends UnboundedPriorityMailbox(
+    PriorityGenerator {
+      case message: String if message.startsWith("URGENT") => 0
+      case _: String => 1
+      case _ => 2
+    }
+  )
+
+
+  val actor = system.actorOf(Props[SimpleActor]().withDispatcher("my-dispatcher"))
+  actor ! "General Kenobi, you are a bold one."
+  actor ! 5
+  actor ! "URGENT Hello, there!"
+
+}
